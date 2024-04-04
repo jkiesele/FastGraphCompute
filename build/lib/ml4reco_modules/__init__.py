d637253bdc11dcf7e7b1dc5b2d4e41947e7f0810
aratey <rateyayman@gmail.com>
2024-04-04 17:50:30 +0200

    first pip package

---
+++ build/lib/ml4reco_modules/__init__.py
@@ -0,0 +1,9 @@
+"""
+ml4reco_modules.
+
+An example python library.
+"""
+
+__version__ = "1.0"
+__author__ = 'Diego'
+__credits__ = 'KIT'
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ml4reco_modules',
+    version='1.0',    
+    description='A example Python package',
+    url='https://github.com/shuds13/pyexample',
+    author='Diego',
+    author_email='shudson@anl.gov',
+    license='BSD 2-clause',
+    packages=['ml4reco_modules'],
+  
+)
