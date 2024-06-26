9b9520e3dc28e8141a13c77004c577c64448d9dd
aratey <aratey@deepthought.etp.kit.edu>
2024-06-26 13:23:48 +0200

    Test for

--- tests/Test_bin_by_coordinates.py
+++ tests/Test_bin_by_coordinates.py
@@ -1,6 +1,15 @@
-import unittest
 import torch
-from bin_by_coordinates_cpu import bin_by_coordinates_cpu, bin_by_coordinates
+import unittest
+import os.path as osp
+
+
+# Load the shared libraries
+cpu_so_file = osp.join(osp.dirname(osp.realpath(__file__)), 'bin_by_coordinates_cpu.so')
+torch.ops.load_library(cpu_so_file)
+
+from bin_by_coordinates_cpu import bin_by_coordinates_cpu
+from bin_by_coordinates_cpu import bin_by_coordinates
+
 
 class TestBinByCoordinatesCPU(unittest.TestCase):
     def setUp(self):
@@ -13,22 +22,59 @@ class TestBinByCoordinatesCPU(unittest.TestCase):
         self.n_per_bin = torch.zeros(8, dtype=torch.int32)
 
     def test_bin_by_coordinates_cpu(self):
-        bin_by_coordinates_cpu(self.coordinates, self.rs, self.bin_width, self.n_bins, self.assigned_bin, self.flat_assigned_bin, self.n_per_bin, True)
-        expected_assigned_bin = torch.tensor([[0, 0, 1], [1, 1, 2], [0, 2, 3]])
-        expected_flat_assigned_bin = torch.tensor([1, 6, 4])
-        expected_n_per_bin = torch.tensor([0, 1, 0, 1, 1, 0, 1, 0])
-        self.assertTrue(torch.equal(self.assigned_bin, expected_assigned_bin))
-        self.assertTrue(torch.equal(self.flat_assigned_bin, expected_flat_assigned_bin))
-        self.assertTrue(torch.equal(self.n_per_bin, expected_n_per_bin))
+        # Convert tensors to Int type
+        rs_int = self.rs.int()
+        bin_width_int = self.bin_width.int()
+        n_bins_int = self.n_bins.int()
+        assigned_bin_int = self.assigned_bin.int()
+        flat_assigned_bin_int = self.flat_assigned_bin.int()
+        n_per_bin_int = self.n_per_bin.int()
+
+        bin_by_coordinates_cpu(self.coordinates, rs_int, bin_width_int, n_bins_int, assigned_bin_int, flat_assigned_bin_int, n_per_bin_int, True)
+        expected_assigned_bin = torch.tensor([[0, 0, 1], [1, 1, 2], [0, 2, 3]], dtype=torch.int32)
+        expected_flat_assigned_bin = torch.tensor([1, 6, 4], dtype=torch.int32)
+        expected_n_per_bin = torch.tensor([0, 1, 0, 1, 1, 0, 1, 0], dtype=torch.int32)
+        self.assertTrue(torch.equal(assigned_bin_int, expected_assigned_bin))
+        self.assertTrue(torch.equal(flat_assigned_bin_int, expected_flat_assigned_bin))
+        self.assertTrue(torch.equal(n_per_bin_int, expected_n_per_bin))
 
     def test_bin_by_coordinates(self):
-        bin_by_coordinates(self.coordinates, self.rs, self.bin_width, self.n_bins, self.assigned_bin, self.flat_assigned_bin, self.n_per_bin, True)
-        expected_assigned_bin = torch.tensor([[0, 0, 1], [1, 1, 2], [0, 2, 3]])
-        expected_flat_assigned_bin = torch.tensor([1, 6, 4])
-        expected_n_per_bin = torch.tensor([0, 1, 0, 1, 1, 0, 1, 0])
-        self.assertTrue(torch.equal(self.assigned_bin, expected_assigned_bin))
-        self.assertTrue(torch.equal(self.flat_assigned_bin, expected_flat_assigned_bin))
-        self.assertTrue(torch.equal(self.n_per_bin, expected_n_per_bin))
+        # Convert tensors to Int type
+        rs_int = self.rs.int()
+        bin_width_int = self.bin_width.int()
+        n_bins_int = self.n_bins.int()
+        assigned_bin_int = self.assigned_bin.int()
+        flat_assigned_bin_int = self.flat_assigned_bin.int()
+        n_per_bin_int = self.n_per_bin.int()
+
+        bin_by_coordinates(self.coordinates, rs_int, bin_width_int, n_bins_int, assigned_bin_int, flat_assigned_bin_int, n_per_bin_int, True)
+        expected_assigned_bin = torch.tensor([[0, 0, 1], [1, 1, 2], [0, 2, 3]], dtype=torch.int32)
+        expected_flat_assigned_bin = torch.tensor([1, 6, 4], dtype=torch.int32)
+        expected_n_per_bin = torch.tensor([0, 1, 0, 1, 1, 0, 1, 0], dtype=torch.int32)
+        self.assertTrue(torch.equal(assigned_bin_int, expected_assigned_bin))
+        self.assertTrue(torch.equal(flat_assigned_bin_int, expected_flat_assigned_bin))
+        self.assertTrue(torch.equal(n_per_bin_int, expected_n_per_bin))
+
 
 if __name__ == '__main__':
     unittest.main()
+    
+    
+
+    # def test_bin_by_coordinates_cpu(self):
+    #     bin_by_coordinates_cpu(self.coordinates, self.rs, self.bin_width, self.n_bins, self.assigned_bin, self.flat_assigned_bin, self.n_per_bin, True)
+    #     expected_assigned_bin = torch.tensor([[0, 0, 1], [1, 1, 2], [0, 2, 3]])
+    #     expected_flat_assigned_bin = torch.tensor([1, 6, 4])
+    #     expected_n_per_bin = torch.tensor([0, 1, 0, 1, 1, 0, 1, 0])
+    #     self.assertTrue(torch.equal(self.assigned_bin, expected_assigned_bin))
+    #     self.assertTrue(torch.equal(self.flat_assigned_bin, expected_flat_assigned_bin))
+    #     self.assertTrue(torch.equal(self.n_per_bin, expected_n_per_bin))
+
+    # def test_bin_by_coordinates(self):
+    #     bin_by_coordinates(self.coordinates, self.rs, self.bin_width, self.n_bins, self.assigned_bin, self.flat_assigned_bin, self.n_per_bin, True)
+    #     expected_assigned_bin = torch.tensor([[0, 0, 1], [1, 1, 2], [0, 2, 3]])
+    #     expected_flat_assigned_bin = torch.tensor([1, 6, 4])
+    #     expected_n_per_bin = torch.tensor([0, 1, 0, 1, 1, 0, 1, 0])
+    #     self.assertTrue(torch.equal(self.assigned_bin, expected_assigned_bin))
+    #     self.assertTrue(torch.equal(self.flat_assigned_bin, expected_flat_assigned_bin))
+    #     self.assertTrue(torch.equal(self.n_per_bin, expected_n_per_bin))
