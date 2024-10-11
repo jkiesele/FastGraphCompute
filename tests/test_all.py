a8684d7eb7cb48963a634205aa7a117a3108c89d
Jan Kieseler <jkiesele@cern.ch>
2024-10-11 09:54:17 +0200

    torch geometric interface

--- tests/test_all.py
+++ tests/test_all.py
@@ -3,6 +3,7 @@ from test_bin_by_coordinates import TestBinByCoordinates
 from test_binned_select_knn import TestBinnedSelectKnn
 from test_index_replacer import TestIndexReplacer
 from test_oc_helper import TestOcHelper
+from test_torch_geometric_interface import TestRowSplitsFromStrictBatch, TestStrictBatchFromRowSplits
 
 
 if __name__ == '__main__':
