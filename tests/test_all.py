d8b43ff5914dd1f7550deb384473cf8ca097895c
Jan Kieseler <jkiesele@cern.ch>
2024-10-16 13:59:31 +0200

    integrated more into all tests

--- tests/test_all.py
+++ tests/test_all.py
@@ -4,6 +4,7 @@ from test_binned_select_knn import TestBinnedSelectKnn
 from test_index_replacer import TestIndexReplacer
 from test_oc_helper import TestOcHelper
 from test_torch_geometric_interface import TestRowSplitsFromStrictBatch, TestStrictBatchFromRowSplits
+from test_gnn_ops import TestGravNetOp
 
 
 if __name__ == '__main__':
