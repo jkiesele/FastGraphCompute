ff6f98d2c809bf0fdd53a0316076400739269bdf
Jan Kieseler <jkiesele@cern.ch>
2026-03-27 14:42:48 +0100

    adjusted version number

--- fastgraphcompute/__init__.py
+++ fastgraphcompute/__init__.py
@@ -6,6 +6,6 @@ from .extensions.oc_helper import oc_helper_matrices, select_with_default
 from .extensions.binned_select_knn import binned_select_knn
 from .extensions.index_replacer import index_replacer
 from .extensions.bin_by_coordinates import bin_by_coordinates
-__version__ = "1.0"
+__version__ = "1.0.1"
 __author__ = ''
 __credits__ = ''
